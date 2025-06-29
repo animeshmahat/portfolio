cf4a32b67ab74e4a3514cbd6d2895fa3eec3f42c
Animesh <mahatanimesh333@gmail.com>
2025-06-29 11:05:07 +0545

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full overflow-hidden">
+      <h1 className="text-3xl font-bold underline text-gray-950">
+        Hello, World
+      </h1>
+    </div>
+  );
+}
